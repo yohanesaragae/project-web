5ef3c53f70e7f6062e8d9a970e9b27a4aab07822
yohanespiteraragae <119453202+yohanesaragae@users.noreply.github.com>
2024-11-05 19:15:53 +0900

    Update all Files

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\User;
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 1)->create();
+    }
+}
